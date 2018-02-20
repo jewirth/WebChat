94e0bf3746f9fb7ff796ea4864e2ff2ba75bdb46
Jens Wirth <jewirth@mailbox.org>
2018-02-20 23:34:28 +0100

    changed alot...

--- client.js
+++ client.js
@@ -16,17 +16,41 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+var window_focus = true;
+var unread = 0;
+
+$(window).focus(function() {
+    window_focus = true;
+	unread = 0;
+	document.title = 'chat';
+}).blur(function() {
+    window_focus = false;
+});
+
 $(document).ready(function(){
+	$('#content').append($('<li></li>').append(
+       	$('<span>').text('Welcome to http://foo.snej.de'),
+	));
+	$('#content').append($('<li></li>').append(
+    	$('<p>').text(' '),
+	));
+
 	var socket = io.connect();
 	socket.on('chat', function (data) {
 	    var zeit = new Date(data.zeit);
+		if (!window_focus) {
+			unread++;
+			document.title = 'chat (' + unread + ')';
+		}
 	    $('#content').append(
 	        $('<li></li>').append(
-	            $('<span>').text(),
+	            $('<span>').text('[' + zeit.getHours() + ':' +
+                 (zeit.getMinutes()<10?+'0':'') + zeit.getMinutes() + ':' +
+                 (zeit.getSeconds()<10?+'0':'') + zeit.getSeconds() + '] '),
 	            $('<b>').text(typeof(data.name) != 'undefined' ? data.name + ': ' : ''),
 	            $('<span>').text(data.text))
 	    );
-			window.scrollTo(0,document.body.scrollHeight);
+		window.scrollTo(0,document.body.scrollHeight);
 	});
 
 	function senden(){
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /*
 WebChat - a node.js web chat
-Copyright (C) 2016  Jens Wirth <jewirth@gmail.com>
+Copyright (C) 2015  Jens Wirth <jewirth@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -19,35 +19,41 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 var express = require('express')
 ,   app = express()
 ,   server = require('http').createServer(app)
-,   io = require('socket.io').listen(server);
+,   io = require('socket.io').listen(server)
+,   fs = require('fs');
 
-var history = [];
+server.listen(80);
+
+var history;
+
+try {
+    history = JSON.parse(fs.readFileSync('last_100_messages', 'utf8'));
+} catch (error) {
+    history = [];
+}
 
 function addToHistory(msg) {
+  msg.isHistory = true;
   history.push(msg);
   if (history.length > 100) {
     history = history.slice(1);
   }
+  fs.writeFileSync("last_100_messages", JSON.stringify(history), 'utf-8');
 }
 
-server.listen(3001);
-
 app.use(express.static(__dirname + '/'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
-app.get('/', function (req, res) {
-	console.log("incomming GET /");
-	res.sendFile(__dirname + '/index.html');
-});
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('chat', { zeit: new Date(), text: 'You are online!!' });
+io.sockets.on('connection', (socket) => {
   for (i=0; i<history.length; i++) {
     socket.emit('chat', history[i]);
   }
-  socket.on('chat', function (msg) {
-    var tosend = { zeit: new Date(), name: msg.name || 'Anonym', text: msg.text };
-    io.sockets.emit('chat', tosend);
-    addToHistory(tosend);
+  socket.on('chat', (msg) => {
+    if (msg.text.length > 0) {
+        var tosend = { zeit: new Date(), name: msg.name || 'Anonym', text: msg.text };
+        io.sockets.emit('chat', tosend);
+        addToHistory(tosend);
+    }
   });
 });
 
